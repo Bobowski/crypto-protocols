1ad399e66b38c6b6d517b4ba5ad61c80851c7448
Adam Bobowski <bobowskiadam@gmail.com>
2017-11-23 13:32:38 +0100

    oblivious transfer 1-of-n

---
+++ oblivious-1-of-n/utils.py
@@ -0,0 +1,32 @@
+from functools import reduce
+
+from charm.core.math.integer import serialize, deserialize
+
+import base64
+import json
+
+
+def product(A):
+    return reduce(lambda x, y: x * y, A)
+
+
+def b64encode(d):
+    if isinstance(d, (list, tuple)):
+        return [b64encode(a) for a in d]
+    if isinstance(d, dict):
+        return {k: b64encode(v) for k, v in d.items()}
+    return base64.b64encode(serialize(d)).decode('UTF-8')
+
+def b64decode(d):
+    if isinstance(d, list):
+        return [b64decode(a) for a in d]
+    if isinstance(d, dict):
+        return {k: b64decode(v) for k, v in d.items()}
+    return deserialize(base64.b64decode(d))
+
+
+def jencode(d):
+    return json.dumps(b64encode(d))
+
+def jdecode(d):
+    return b64decode(json.loads(d))
